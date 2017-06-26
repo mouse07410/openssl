81ceb8f244def1cb16b7218d9293b8af52481e18
Mouse <mouse008@gmail.com>
2017-06-26 18:49:28 -0400

    Changed hard-coded config to HOME-dependent

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
-./config --prefix=$HOME/openssl-1.1 --openssldir=/Users/ur20980/openssl-1.1/etc enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic
+./config --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic
 make depend && make clean && make -j 4 all && make test && make install
 
