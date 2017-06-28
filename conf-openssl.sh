25d003c4e9a721ac4a8b4949861f92c22ec3d3bc
Mouse <mouse008@gmail.com>
2017-06-27 21:19:11 -0400

    Added "make distclean" and made sure the build proceeds even if distclean fails

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
 
+make distclean || true
 ./config --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic
 make depend && make clean && make -j 4 all && make test && make install
 
