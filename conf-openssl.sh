c0983e7c801e7b96aa3693fbb38c83d1b0418a24
Mouse <mouse008@gmail.com>
2017-08-07 21:04:23 -0400

    Reduced # of parallel make runs from 4 to 2

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -5,5 +5,5 @@ make distclean || true
 #./config --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
 # For OpenSSL-1.1.0-stable
 ./config --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
-make depend && make clean && make -j 4 all && make test && make install
+make depend && make clean && make -j 2 all && make test && make install
 
