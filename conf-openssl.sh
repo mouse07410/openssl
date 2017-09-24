36e9cb28646121f898dcf15b44385408741225c5
Mouse <mouse008@gmail.com>
2017-09-24 15:45:57 -0400

    Build OpenSSL_1_1_0-stable with debugging symbols

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -2,8 +2,8 @@
 
 make distclean || true
 # For OpenSSL-1.1.1-dev
-#./config --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
+#./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
 # For OpenSSL-1.1.0-stable
-./config --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
-make depend && make clean && make -j 2 all && make test && make install
+./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
+make depend && make clean && CFLAGS="$CFLAGS -g" LDFLAGS="-g $LDFLAGS" make -j 2 all && make test && make install
 
