805505500667b18d3595826b806af318860aa6da
Mouse <mouse008@gmail.com>
2017-10-03 22:17:31 -0400

    Made the auto-config match Macports

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -6,6 +6,6 @@ make distclean || true
 # For OpenSSL-1.1.0-stable
 #./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
 # For OpenSSL-1.1.0-stable
-./config --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-rfc3779 experimental-jpake
+./config --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl enable-rfc3779
 
 make depend && make clean && CFLAGS="$CFLAGS -g" LDFLAGS="-g $LDFLAGS" make -j 2 all && make test && make install
