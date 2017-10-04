87665021018b20603eab2bea5c7bd22d3e089dcb
Mouse <mouse008@gmail.com>
2017-10-03 23:07:46 -0400

    Fixed the auto-script to build 64-bit binaries

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -ex
 
 make distclean || true
+
 # For OpenSSL-1.1.1-dev
 #./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
 # For OpenSSL-1.1.0-stable
 #./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
 # For OpenSSL-1.1.0-stable
-./config darwin64-x86_64-cc --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl enable-rfc3779
+./Configure darwin64-x86_64-cc shared enable-rfc3779 --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl 
 
-make depend && make clean && CFLAGS="$CFLAGS -g" LDFLAGS="-g $LDFLAGS" make -j 2 all && make test && make install
+make depend && make clean && CFLAGS="$CFLAGS -g" LDFLAGS="-g $LDFLAGS" make -j 2 all && make test && sudo make install
