ab6624f09d37cf66c1b588d436ffb6ddb11af3c5
Mouse <mouse008@gmail.com>
2017-10-03 21:58:15 -0400

    Adding convenient auto config+build script

---
+++ conf-openssl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+make distclean || true
+# For OpenSSL-1.1.1-dev
+#./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
+# For OpenSSL-1.1.0-stable
+#./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
+# For OpenSSL-1.1.0-stable
+./config --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-rfc3779 experimental-jpake
+
+make depend && make clean && CFLAGS="$CFLAGS -g" LDFLAGS="-g $LDFLAGS" make -j 2 all && make test && make install
