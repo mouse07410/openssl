0c023441add05d224aa4b6768f46112291738b8a
Mouse <mouse008@gmail.com>
2017-10-03 22:45:54 -0400

    Do not ignore provided context in EVP_DigestSignInit()

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -5,5 +5,7 @@ make distclean || true
 #./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
 # For OpenSSL-1.1.0-stable
 ./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
-make depend && make clean && CFLAGS="$CFLAGS -g" LDFLAGS="-g $LDFLAGS" make -j 2 all && make test && make install
+# For OpenSSL-1.1.0-stable
+#./config --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl enable-rfc3779
 
+make depend && make clean && CFLAGS="$CFLAGS -g" LDFLAGS="-g $LDFLAGS" make -j 2 all && make test && make install
