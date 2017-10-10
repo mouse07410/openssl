0a53ba25e5152a7421a665ecfc90954d038e43e1
Mouse <mouse008@gmail.com>
2017-10-10 18:50:38 -0400

    Added --debug to config

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -ex
 
 make distclean || true
+
 # For OpenSSL-1.1.1 master (development branch)
-./config --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc --with-rand-seed=rdcpu enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
+./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc --with-rand-seed=rdcpu enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
 # For OpenSSL-1.1.0-stable
-#./config --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
+#./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
+
 make depend && make clean && make -j 2 all && make test && make install
 
