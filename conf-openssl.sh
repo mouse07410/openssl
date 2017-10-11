f1cd1666bb9f8ad96781e82bad85f552764f0696
Mouse <mouse008@gmail.com>
2017-10-11 09:59:36 -0400

    Added --debug to 1.0.2 auto-build

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -9,7 +9,7 @@ make distclean || true
 #./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
 
 # For OpenSSL-1.0.2-stable
-#./Configure darwin-x86_64-cc --prefix=/opt/local --openssldir=/opt/local/etc/openssl shared threads enable-rfc3779
+#./Configure darwin-x86_64-cc --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl shared threads enable-rfc3779
 
 make depend && make clean && make -j 2 all && make test && make install
 
