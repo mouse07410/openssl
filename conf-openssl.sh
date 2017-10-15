7897c47ec6b10cd698ff003c90e1b299f149a764
Mouse <mouse008@gmail.com>
2017-10-14 21:58:51 -0400

    Updated auto-build script

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -ex
 
 make distclean || true
-# For OpenSSL-1.1.1-dev
-#./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
+
+# For OpenSSL-1.1.1 master (development branch)
+#./config --prefix=$HOME/openssl-1.1 --debug --openssldir=$HOME/openssl-1.1/etc --with-rand-seed=rdcpu enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
+
 # For OpenSSL-1.1.0-stable
 ./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
-# For OpenSSL-1.1.0-stable
-#./config --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl enable-rfc3779
 
-make depend && make clean && CFLAGS="$CFLAGS -g" LDFLAGS="-g $LDFLAGS" make -j 2 all && make test && make install
+# For OpenSSL-1.0.2-stable
+#./Configure darwin-x86_64-cc --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl shared threads enable-rfc3779
+
+make depend && make clean && make -j 2 all && make test && make install
+
