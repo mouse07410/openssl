e77509b2a7f71f4cd9c1eeb717ffeede12f98515
Mouse <mouse008@gmail.com>
2018-09-12 08:26:33 -0400

    Added "enable-ssl-trace" to auto-config

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -3,11 +3,11 @@
 make distclean || true
 
 # For OpenSSL-1.1.1 master (development branch)
-./config --prefix=$HOME/openssl-1.1 --debug --openssldir=$HOME/openssl-1.1/etc --with-rand-seed=rdcpu enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic 
+./config --prefix=$HOME/openssl-1.1 --debug --openssldir=$HOME/openssl-1.1/etc --with-rand-seed=rdcpu enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-ssl-trace
 #enable-tls1_3 enable-tls13downgrade
 
 # For OpenSSL-1.1.0-stable
-#./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade
+#./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade enable-ssl-trace
 
 # For OpenSSL-1.0.2-stable
 #./Configure debug-darwin64-x86_64-cc --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl shared threads enable-rfc3779
