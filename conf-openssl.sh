bd8291a2432b21a468226dc097b5ff60797b224e
Mouse <mouse008@gmail.com>
2020-05-02 21:22:49 -0400

    Updated the auto config- and build-file to the current OpenSSL-3.0 alpha-2 changes

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -15,8 +15,7 @@ CFLAGS="$CFLAGS -g"
 make distclean || true
 
 # For OpenSSL-3 master (development branch)
-./config --prefix=${OPENSSL_DIR} --debug --openssldir=${OPENSSL_DIR}/etc --with-rand-seed=rdcpu enable-aria enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-ssl-trace
-#enable-tls1_3 enable-tls13downgrade
+./config --prefix=${OPENSSL_DIR} --debug --openssldir=${OPENSSL_DIR}/etc --with-rand-seed=rdcpu,os enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers zlib-dynamic enable-ssl-trace enable-trace threads enable-buildtest-c++
 
 # For OpenSSL-1.1.1-stable
 #./config --debug --prefix=$HOME/openssl-1.1 --openssldir=$HOME/openssl-1.1/etc enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers enable-zlib-dynamic enable-tls1_3 enable-tls13downgrade enable-ssl-trace
@@ -24,5 +23,6 @@ make distclean || true
 # For OpenSSL-1.0.2-stable
 #./Configure debug-darwin64-x86_64-cc --debug --prefix=/opt/local --openssldir=/opt/local/etc/openssl shared threads enable-rfc3779
 
-make update && make depend && make -j 2 all 2>&1 | tee make-out.txt && OPENSSL_GOST_ENGINE_SO="${OPENSSL_GOST_ENGINE_SO}" make test 2>&1 | tee test-out.txt  && make install && cp apps/openssl-3.cnf $OPENSSL_DIR/etc/openssl.cnf && tail test-out.txt
+#make update && 
+make depend && make -j 2 all 2>&1 | tee make-out.txt && OPENSSL_GOST_ENGINE_SO="${OPENSSL_GOST_ENGINE_SO}" make test 2>&1 | tee test-out.txt  && make install 2>&1 | tee install-out.txt && cp apps/openssl-3.cnf $OPENSSL_DIR/etc/openssl.cnf && tail test-out.txt
 
