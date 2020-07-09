dbecca48dcf6446954ca15e1f232d377deae31c3
Mouse <mouse008@gmail.com>
2020-07-08 21:58:47 -0400

    Add DTG (Date/Time Group) to the beginning and end of the process

--- conf-openssl.sh
+++ conf-openssl.sh
@@ -16,6 +16,8 @@ export KERNEL_BITS=64
 
 make distclean || true
 
+date
+
 # For OpenSSL-3 master (development branch)
 ./config --prefix=${OPENSSL_DIR} --debug --openssldir=${OPENSSL_DIR}/etc --with-rand-seed=rdcpu,os enable-ec_nistp_64_gcc_128 enable-md2 enable-rc5 enable-weak-ssl-ciphers zlib-dynamic enable-ssl-trace enable-trace threads enable-buildtest-c++
 
@@ -28,3 +30,4 @@ make distclean || true
 #make update && 
 time (make depend && make -j 4 all 2>&1 | tee make-out.txt && OPENSSL_GOST_ENGINE_SO="${OPENSSL_GOST_ENGINE_SO}" make -j4 test 2>&1 | tee test-out.txt  && make install 2>&1 | tee install-out.txt && cp apps/openssl-3.cnf $OPENSSL_DIR/etc/openssl.cnf && tail test-out.txt)
 
+date
